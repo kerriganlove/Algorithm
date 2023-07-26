18b5bc4aaca9f64bd32392136fb852721db290a0
정대산 <54668923+kerriganlove@users.noreply.github.com>
2023-07-26 14:46:41 +0900

    [level 2] Title: n^2 배열 자르기, Time: 18.44 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/lv2/87390. n＾2 배열 자르기/n＾2 배열 자르기.kt
+++ 프로그래머스/lv2/87390. n＾2 배열 자르기/n＾2 배열 자르기.kt
@@ -1,15 +1,17 @@
 class Solution {
     fun solution(n: Int, left: Long, right: Long): IntArray {
         var answer: IntArray = intArrayOf()
+        var list = mutableListOf<Int>()
         for ( i in left..right ) {
             var div = (i / n) + 1
             var mod = (i % n) + 1
             if ( div >= mod ) {
-                answer += div.toInt()
+                list.add(div.toInt())
             } else {
-                answer += (div + (mod - div)).toInt()
+                list.add(mod.toInt())
             }
         }
+        answer = list.toIntArray()
         return answer
     }
 }
